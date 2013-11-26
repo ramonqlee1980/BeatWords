dc26048776e1164681d6b89b5a7f2fa57954971e
ramonqlee1980 <ramonqlee1980@gmail.com>
2013-11-27 06:50:36 +0800

    init commit
    init commit

---
+++ BeatWords/RMAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  RMAppDelegate.h
+//  BeatWords
+//
+//  Created by Ramonqlee on 11/27/13.
+//  Copyright (c) 2013 iDreems. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RMAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ BeatWordsTests/BeatWordsTests.m
@@ -0,0 +1,34 @@
+//
+//  BeatWordsTests.m
+//  BeatWordsTests
+//
+//  Created by Ramonqlee on 11/27/13.
+//  Copyright (c) 2013 iDreems. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface BeatWordsTests : XCTestCase
+
+@end
+
+@implementation BeatWordsTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
